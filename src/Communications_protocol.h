23c7fa346d69eb20a3003a6fb123d5a92f5b7c70
Noteolvides <gustavogomezlopezdesanroman@gmail.com>
2023-04-05 17:15:34 +0200

    Revert "revert: commit of branch"
    This reverts commit 3f4e4585accafeba6477bc84d2fec85e9a8de432.

--- src/Communications_protocol.h
+++ src/Communications_protocol.h
@@ -8,11 +8,8 @@ enum Devices : uint8_t {
   UNKNOWN = 0,
   KEYSCANNER_DEFY_LEFT,
   KEYSCANNER_DEFY_RIGHT,
-  RF_DEFY_LEFT,
-  RF_DEFY_RIGHT,
-  NEURON_DEFY,
-  RF_NEURON_DEFY,
-  WIRED_NEURON_DEFY,
+  NEURON_DEFY_WIRED,
+  NEURON_DEFY_WIRELESS,
 };
 
 static_assert(sizeof(Devices) == sizeof(uint8_t));
