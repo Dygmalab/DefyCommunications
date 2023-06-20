e4d3564828b81b16fe40a403257ed0cb82d9dbc1
Gonzalo Lopez Di Paola <71147461+gonza2206@users.noreply.github.com>
2023-06-20 03:45:02 -0300

    chore: Added new Command RF_MAC_ADDRESS (#6)
    * chore: Added new Command
    
    Signed-off-by: gonza2206 <gonzalopez2206@gmail.com>
    
    * feat: rf command
    
    Signed-off-by: Noteolvides <gustavogomezlopezdesanroman@gmail.com>
    
    ---------
    
    Signed-off-by: gonza2206 <gonzalopez2206@gmail.com>
    Signed-off-by: Noteolvides <gustavogomezlopezdesanroman@gmail.com>
    Co-authored-by: Noteolvides <gustavogomezlopezdesanroman@gmail.com>

--- src/Communications_protocol.h
+++ src/Communications_protocol.h
@@ -46,11 +46,7 @@ enum Commands : uint8_t {
   BATTERY_STATUS,
   BATTERY_SAVING,
   //Config
-  SET_SPI_SPEED = 100,
-  SET_CLOCK_SPEED,
-  SET_LED_DRIVER_PULLUP,
-  SET_ENABLE_UNDERGLOW,
-  SET_ENABLE_LED_DRIVER,
+  RF_ADDRESS = 50,
 };
 
 static_assert(sizeof(Commands) == sizeof(uint8_t));
