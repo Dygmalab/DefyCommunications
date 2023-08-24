1f6f80dc777550a3b58ce8b94be3d0312b02b2eb
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2023-08-24 15:45:19 +0200

    N2 arduino integration (#14)
    * chore: removing usb_composite_dev
    
    Signed-off-by: noteolvides <gustavogomezlopezdesanroman@gmail.com>
    
    * feat: added tinusb
    
    Signed-off-by: noteolvides <gustavogomezlopezdesanroman@gmail.com>
    
    * refactor: change ready to mounted
    
    Signed-off-by: noteolvides <gustavogomezlopezdesanroman@gmail.com>
    
    ---------
    
    Signed-off-by: noteolvides <gustavogomezlopezdesanroman@gmail.com>

--- src/device/defyN2/CommunicationsN2.cpp
+++ src/device/defyN2/CommunicationsN2.cpp
@@ -2,16 +2,12 @@
 #include "Communications.h"
 #include "Communications_protocol_rf.h"
 #include "SpiPort.h"
-#include "Time_counter.h"
-#include "Usb_serial.h"
 #include "rfgw_config_app.h"
 #include "rf_gateway.h"
 #include "CRC_wrapper.h"
 #include "Arduino.h"
+#include "Adafruit_USBD_Device.h"
 
-extern Usb_serial usb_serial;  // It is declared in main.cpp
-
-extern Time_counter time_counter;
 
 constexpr static uint8_t DELIMITER{0b10101010};
 
@@ -40,7 +36,7 @@ void checkActive();
 class RFGW_parser {
  public:
   static void run() {
-    if (!usbd_ready()) return;
+    if (!TinyUSBDevice.mounted()) return;
     rfgw_poll();
     left.run();
     right.run();
@@ -149,7 +145,7 @@ class RFGW_parser {
     uint64_t lastTimeCommunicationSend{0};
     std::queue<Communications_protocol_rf::WrapperPacket> tx_messages;
     void sendPacket(Packet &packet) {
-      if (!usbd_ready()) return;
+      if (!TinyUSBDevice.mounted()) return;
       packet.header.crc    = 0;
       packet.header.device = Communications_protocol::RF_NEURON_DEFY;
       packet.header.crc    = crc8(packet.buf, sizeof(Header) + packet.header.size);
@@ -215,7 +211,7 @@ void Communications::run() {
 bool Communications::sendPacket(Packet packet) {
   Devices device_to_send = packet.header.device;
   if (device_to_send == UNKNOWN) {
-    if (usbd_ready()) {
+    if (TinyUSBDevice.mounted()) {
 #if COMPILE_SPI0_SUPPORT
       if (spiPort0Device != UNKNOWN)
         spiPort0.sendPacket(packet);
@@ -251,7 +247,7 @@ bool Communications::sendPacket(Packet packet) {
     return true;
   }
 
-  if (usbd_ready()) {
+  if (TinyUSBDevice.mounted()) {
 
 #if COMPILE_SPI0_SUPPORT
     if (spiPort0Device == device_to_send)
