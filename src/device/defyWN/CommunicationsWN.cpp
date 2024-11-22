d7bee2e525e1942b77e1c00bc5862af8d003735d
gonza2206 <gonzalopez2206@gmail.com>
2024-11-22 10:51:28 -0300

    refactor: Wired Neuron Communications
    Signed-off-by: gonza2206 <gonzalopez2206@gmail.com>

--- src/device/defyWN/CommunicationsWN.cpp
+++ src/device/defyWN/CommunicationsWN.cpp
@@ -20,9 +20,26 @@
 #ifdef ARDUINO_ARCH_RP2040
 
 #include "Communications.h"
-#include "SPISlave.h"
+#include "SpiPort.h"
+#include <kaleidoscope.h>
+
+
+#define DEBUG_LOG_N2_COMMUNICATIONS 0
+
+#define PORT_IS_ALIVE_TIMEOUT_MS    2000
+
+static SpiPort spiPort1(0);
+static Devices spiPort1Device{Communications_protocol::UNKNOWN};
+static uint32_t spiPort1LastCommunication{0};
+
+static SpiPort spiPort2(1);
+static Devices spiPort2Device{Communications_protocol::UNKNOWN};
+static uint32_t spiPort2LastCommunication{0};
+
+/****************************************************************** */
 
 constexpr static uint32_t timeout = 600;
+
 struct SideInfo {
   SideInfo(Devices _devices)
     : device(_devices) {}
@@ -37,82 +54,178 @@ static SideInfo right{Communications_protocol::KEYSCANNER_DEFY_RIGHT};
 
 void checkActive(SideInfo &side);
 
+
+/****************************************************************** */
+
+
+class WiredCommunications {
+ public:
+  static void init() 
+  {
+    spiPort1.init();
+    spiPort2.init();
+  }
+
+  static void readPacket(uint8_t port) 
+  {
+    SpiPort &spiPort             = port == 0 ? spiPort1 : spiPort2;
+    Devices &device              = port == 0 ? spiPort1Device : spiPort2Device;
+    uint32_t &lastCommunications = port == 0 ? spiPort1LastCommunication : spiPort2LastCommunication;
+    Packet packet{};
+    
+    if (spiPort.readPacket(packet)) 
+    {
+      lastCommunications = millis();
+      device             = packet.header.device;
+      Communications.callbacks.call(packet.header.command, packet);
+    }
+  }
+
+  static bool isPortAlive(uint8_t port) {
+    uint32_t &lastCommunications = port == 0 ? spiPort1LastCommunication : spiPort2LastCommunication;
+
+    /* Check if there was any communication at all */
+    if (lastCommunications == 0) {
+      return false;
+    }
+
+    /* Check how long it is since the last communication */
+    return ((millis() - lastCommunications) < PORT_IS_ALIVE_TIMEOUT_MS) ? true : false;
+  }
+
+  static bool isPortLeftAlive(void) {
+    if (spiPort1Device == KEYSCANNER_DEFY_LEFT) {
+      return isPortAlive(0);
+    } else if (spiPort2Device == KEYSCANNER_DEFY_LEFT) {
+      return isPortAlive(1);
+    } else {
+      return false;
+    }
+  }
+
+  static bool isPortRightAlive(void) {
+    if (spiPort1Device == KEYSCANNER_DEFY_RIGHT) {
+      return isPortAlive(0);
+    } else if (spiPort2Device == KEYSCANNER_DEFY_RIGHT) {
+      return isPortAlive(1);
+    } else {
+      return false;
+    }
+  }
+
+  static void disconnect(uint8_t port) {
+    SpiPort &spiPort = port == 0 ? spiPort1 : spiPort2;
+    Devices &device  = port == 0 ? spiPort1Device : spiPort2Device;
+    Packet packet{};
+
+    packet.header.command = Communications_protocol::DISCONNECTED;
+    packet.header.device  = device;
+    Communications.callbacks.call(packet.header.command, packet);
+    device = UNKNOWN;
+    //Remove all the left packets at disconnections
+    spiPort.clearRead();
+    spiPort.clearSend();
+  }
+
+  static void portRun(uint8_t port) {
+    SpiPort &spiPort = port == 0 ? spiPort1 : spiPort2;
+
+    spiPort.run();
+  }
+
+  static void run() {
+
+    auto const &keyScanner       = kaleidoscope::Runtime.device().keyScanner();
+    static bool wasLeftConnected = false;
+    auto isDefyLeftWired         = keyScanner.leftSideWiredConnection();
+    if (isDefyLeftWired) {
+      portRun(0);
+      readPacket(0);
+    }
+    if (wasLeftConnected && !isDefyLeftWired) {
+      disconnect(0);
+    }
+    wasLeftConnected = isDefyLeftWired;
+
+    static bool wasRightConnected = false;
+    auto isDefyRightWired         = keyScanner.rightSideWiredConnection();
+    if (isDefyRightWired) {
+      portRun(1);
+      readPacket(1);
+    }
+    if (wasRightConnected && !isDefyRightWired) {
+      disconnect(1);
+    }
+    wasRightConnected = isDefyRightWired;
+  }
+};
+
+void Communications::get_keyscanner_configuration(uint8_t side) {
+
+  Communications_protocol::Packet p{};
+  //p.header.device  = static_cast<Devices>(side);
+  p.header.size    = 1;
+  p.header.command = Communications_protocol::CONFIGURATION;
+  sendPacket(p);
+}
+
 void Communications::init() {
-  port0.init();
-  port1.init();
 
   callbacks.bind(CONNECTED, [this](Packet p) {
     p.header.size    = 0;
     p.header.device  = p.header.device;
     p.header.command = CONNECTED;
     sendPacket(p);
+
+    get_keyscanner_configuration(Devices::KEYSCANNER_DEFY_LEFT);
+    get_keyscanner_configuration(Devices::KEYSCANNER_DEFY_RIGHT);
   });
+
+  WiredCommunications::init();
 }
 
 void Communications::run() {
 
-
-  if (!queue_is_empty(&port0.rx_messages_)) {
-    Packet packet;
-    queue_remove_blocking(&port0.rx_messages_, &packet);
-    SideInfo &side         = packet.header.device == KEYSCANNER_DEFY_LEFT ? left : right;
-    side.lastCommunication = millis();
-    side.online            = true;
-    side.port              = false;
-    callbacks.call(packet.header.command, packet);
-  }
-
-  if (!queue_is_empty(&port1.rx_messages_)) {
-    Packet packet;
-    queue_remove_blocking(&port1.rx_messages_, &packet);
-    SideInfo &side         = packet.header.device == KEYSCANNER_DEFY_LEFT ? left : right;
-    side.lastCommunication = millis();
-    side.online            = true;
-    side.port              = true;
-    callbacks.call(packet.header.command, packet);
-  }
-
-  checkActive(left);
-  checkActive(right);
+  WiredCommunications::run();
 }
 
 bool Communications::sendPacket(Packet packet) {
   Devices device_to_send = packet.header.device;
-  packet.header.device   = Communications_protocol::WIRED_NEURON_DEFY;
-  if (device_to_send == Communications_protocol::UNKNOWN) {
-    if (port0.device != UNKNOWN)
-      queue_add_blocking(&port0.tx_messages_, &packet);
-    if (port1.device != UNKNOWN)
-      queue_add_blocking(&port1.tx_messages_, &packet);
+
+  if (device_to_send == UNKNOWN) {
+
+    if (spiPort1Device != UNKNOWN) {
+      packet.header.device = Communications_protocol::NEURON_DEFY;
+      spiPort1.sendPacket(packet);
+    }
+
+    if (spiPort2Device != UNKNOWN) {
+      packet.header.device = Communications_protocol::NEURON_DEFY;
+      spiPort2.sendPacket(packet);
+    }
+    return true;
   }
-  if (device_to_send == port1.device)
-    queue_add_blocking(&port1.tx_messages_, &packet);
-  if (device_to_send == port0.device)
-    queue_add_blocking(&port0.tx_messages_, &packet);
+
+  if (spiPort1Device == device_to_send) {
+    packet.header.device = Communications_protocol::NEURON_DEFY;
+    spiPort1.sendPacket(packet);
+  }
+
+  if (spiPort2Device == device_to_send) {
+    packet.header.device = Communications_protocol::NEURON_DEFY;
+    spiPort2.sendPacket(packet);
+  }
 
   return true;
 }
 
-void checkActive(SideInfo &side) {
-  if (!side.online) return;
-  const bool now_active = millis() - side.lastCommunication <= timeout;
-
-  if (side.online && !now_active) {
-    side.online   = now_active;
-    SPISlave &spi = side.port ? port1 : port0;
-    spi.device    = Communications_protocol::UNKNOWN;
-    //Clear the packets as now the channel is no longer active
-    Packet packet;
-    while (!queue_is_empty(&spi.rx_messages_)) {
-      queue_remove_blocking(&spi.rx_messages_, &packet);
-    }
-    while (!queue_is_empty(&spi.tx_messages_)) {
-      queue_remove_blocking(&spi.tx_messages_, &packet);
-    }
-    return;
-  }
+bool Communications::isWiredLeftAlive() {
+  return WiredCommunications::isPortLeftAlive();
 }
 
+bool Communications::isWiredRightAlive() {
+  return WiredCommunications::isPortRightAlive();
+}
 
 class Communications Communications;
 
